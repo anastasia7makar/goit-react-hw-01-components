d6474bc93bfd65b692735d77417203d0f8524385
anastasia7makar <anastasia7makar@gmail.com>
2023-07-24 01:19:46 +0300

    Fixed bug build

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,22 @@
 import PropTypes from 'prop-types';
 
 export const Statistics = props => {
-  {
-    return (
-      <section className="statistics">
-        {props.title && <h2 className="title">{props.title}</h2>}
+  return (
+    <section className="statistics">
+      {props.title && <h2 className="title">{props.title}</h2>}
 
-        <ul className="stat-list">
-          {props.stats.map(({ id, label, percentage }) => {
-            return (
-              <li key={id} className="item">
-                <span className="label">{label} </span>
-                <span className="percentage">{percentage}%</span>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    );
-  }
+      <ul className="stat-list">
+        {props.stats.map(({ id, label, percentage }) => {
+          return (
+            <li key={id} className="item">
+              <span className="label">{label} </span>
+              <span className="percentage">{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 };
 
 Statistics.propTypes = {
